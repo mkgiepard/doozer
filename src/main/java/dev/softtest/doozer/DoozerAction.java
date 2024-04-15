787e22765a12ab43dcd83affcd10c7a7148f18cc
Mariusz <mkgiepard@gmail.com>
2024-04-15 16:47:40 +0200

    refactor> move getBySelecter to DoozerAction

--- src/main/java/dev/softtest/doozer/DoozerAction.java
+++ src/main/java/dev/softtest/doozer/DoozerAction.java
@@ -1,11 +1,15 @@
 package dev.softtest.doozer;
 
-public class DoozerAction {
-    private String actionName;
-    private String selector;
-    private String options;
+import java.lang.reflect.Method;
 
-    DoozerAction(String actionName, String selector, String options) {
+import org.openqa.selenium.By;
+
+public class DoozerAction {
+    public String actionName;
+    public String selector;
+    public String options;
+
+    public DoozerAction(String actionName, String selector, String options) {
         this.actionName = actionName;
         this.selector = selector;
         this.options = options;
@@ -28,5 +32,21 @@ public class DoozerAction {
         return "actionName: " + actionName + "\n"
                 + "selector: " + selector + "\n"
                 + "options: " + options + "\n";
+    }
+
+    public By getBySelector(String s) throws Exception {
+
+        // Selector format examples:
+        // - By.cssSelector('button')
+        // - By.name('my-text')
+
+        String methodName = s.substring(s.indexOf(".") + 1, s.indexOf("("));
+        String methodParam = s.substring(s.indexOf("'") + 1, s.length() - 2);
+
+        System.out.println(methodName);
+        System.out.println(methodParam);
+
+        Method bySelectorMethod = By.class.getDeclaredMethod(methodName, String.class);
+        return (By) bySelectorMethod.invoke(null, methodParam);
     }
 }
--- src/main/java/dev/softtest/doozer/actions/Click.java
+++ src/main/java/dev/softtest/doozer/actions/Click.java
@@ -1,20 +1,16 @@
 package dev.softtest.doozer.actions;
 
-import java.lang.reflect.Method;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class Click implements IAction {
+import dev.softtest.doozer.DoozerAction;
+
+public class Click extends DoozerAction implements IAction {
     WebDriver driver;
-    String selector;
-    String options;
 
     public Click(WebDriver driver, String selector, String options) {
+        super("click", selector, options);
         this.driver = driver;
-        this.selector = selector;
-        this.options = options;
     }
 
     public void execute() {
@@ -23,19 +19,7 @@ public class Click implements IAction {
             submitButton.click();
         } catch (Exception e) {
             System.out.println("ups...");
+            System.out.println(e);
         }
-    }
-
-    private By getBySelector(String s) throws Exception {
-
-        // Selector format examples:
-        // - By.cssSelector('button')
-        // - By.name('my-text')
-
-        String methodName = s.substring(s.indexOf(".") + 1, s.indexOf("("));
-        String methodParam = s.substring(s.indexOf("'") + 1, s.length() - 2);
-
-        Method bySelectorMethod = By.class.getDeclaredMethod(methodName, String.class);
-        return (By) bySelectorMethod.invoke(null, methodParam);
     }
 }
