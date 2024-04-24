0939095a28a3ff3acb809f9729a990af41032b14
Mariusz <mkgiepard@gmail.com>
2024-04-24 17:14:15 +0200

    refactor> move parser code to a dedicated class

---
+++ src/main/java/dev/softtest/doozer/Parser.java
@@ -0,0 +1,86 @@
+package dev.softtest.doozer;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.openqa.selenium.WebDriver;
+
+public class Parser {
+    private String filePath;
+    private Charset charset = StandardCharsets.UTF_8;
+    private final String splitter = " \"";
+    private List<DoozerAction> actions = new ArrayList<DoozerAction>();
+    private WebDriver driver;
+
+    public Parser(String filePath, WebDriver driver) {
+        this.filePath = filePath;
+        this.driver = driver;
+    }
+
+    public List<DoozerAction> parse() throws Exception {
+        Path path = FileSystems.getDefault().getPath(this.filePath).toAbsolutePath();
+
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            String line = null;
+            int lineCounter = 1;
+            while ((line = reader.readLine()) != null) {
+                if (line.length() == 0) {
+                    System.out.println(Integer.toString(lineCounter++) + ": ...empty line");
+                    continue;
+                }
+                DoozerAction action = parseAction(lineCounter++, line);
+                actions.add(action);
+            }
+        } catch (Exception e) {
+            System.err.format("Exception: %s%n", e);
+            throw e;
+        }
+        return actions;
+    }
+
+    public DoozerAction parseAction(int lineNumber, String line) throws Exception {
+        DoozerAction action;
+        String[] splitLine = line.split(splitter);
+
+        String name = null;
+        String selector = null;
+        String options = null;
+        if (splitLine.length == 3) {
+            name = splitLine[0].replaceAll("\"", "");
+            selector = splitLine[1].replaceAll("\"", "");
+            options = splitLine[2].replaceAll("\"", "");
+        } else if (splitLine.length == 2) {
+            name = splitLine[0].replaceAll("\"", "");
+            selector = splitLine[1].replaceAll("\"", "");
+        } else if (splitLine.length == 1) {
+            name = splitLine[0].replaceAll("\"", "");
+        }
+        
+        try {
+            action = createActionInstance(name, selector, options);
+        } catch (Exception e) {
+            System.err.format("Exception: %s%n", e);
+            throw e;
+        }
+        action.setLineNumber(lineNumber);
+        return action;
+    }
+
+    private DoozerAction createActionInstance(String actionName, String actionSelector, String actionOptions)
+            throws Exception {
+        String actionClassPrefix = "dev.softtest.doozer.actions.";
+        String actionClassName = actionName.substring(0, 1).toUpperCase()
+                + actionName.substring(1);
+        return (DoozerAction) Class.forName(actionClassPrefix +
+                actionClassName).getConstructor(WebDriver.class, String.class,
+                        String.class, String.class)
+                .newInstance(driver, actionName, actionSelector, actionOptions);
+    }
+}
--- src/test/java/dev/softtest/doozer/DoozerTest.java
+++ src/test/java/dev/softtest/doozer/DoozerTest.java
@@ -1,72 +1,32 @@
 package dev.softtest.doozer;
 
-import java.io.BufferedReader;
 import java.util.stream.*;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class DoozerTest {
     public static WebDriver driver;
-    private static String doozerTestFile = "src/test/java/dev/softtest/doozer/scripts/firstTest.doozer";
-    private static ArrayList<DoozerAction> actions = new ArrayList<DoozerAction>();
-    private static final String splitterChar = " \"";
+    private static List<DoozerAction> actions = new ArrayList<DoozerAction>();
 
     // @BeforeAll
+    // hint: https://code-case.hashnode.dev/how-to-pass-parameterized-test-parameters-to-beforeeachaftereach-method-in-junit5
+    // hint: https://stackoverflow.com/questions/62036724/how-to-parameterize-beforeeach-in-junit-5
     public static void setup(String testFile) throws Exception {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
-        Charset charset = StandardCharsets.UTF_8;
-        Path path = FileSystems.getDefault().getPath(testFile).toAbsolutePath();
-
-        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
-            String line = null;
-            int lineCounter = 1;
-            while ((line = reader.readLine()) != null) {
-                String[] splitLine = line.split(splitterChar);
-
-                String name = null;
-                String selector = null;
-                String options = null;
-                if (splitLine.length == 3) {
-                    name = splitLine[0].replaceAll("\"", "");
-                    selector = splitLine[1].replaceAll("\"", "");
-                    options = splitLine[2].replaceAll("\"", "");
-                } else if (splitLine.length == 2) {
-                    name = splitLine[0].replaceAll("\"", "");
-                    selector = splitLine[1].replaceAll("\"", "");
-                } else if (splitLine.length == 1) {
-                    name = splitLine[0].replaceAll("\"", "");
-                }
-                if (name == null || name.length() == 0) {
-                    System.out.println(Integer.toString(lineCounter++) + ": ...empty line");
-                    continue;
-                }
-                DoozerAction action = createActionInstance(name, selector, options);
-                action.setLineNumber(lineCounter++);
-                actions.add(action);
-            }
-        } catch (Exception e) {
-            System.err.format("Exception: %s%n", e);
-            throw e;
-        }
+        Parser p = new Parser(testFile, driver);
+        actions = p.parse();
     }
 
     @AfterEach
@@ -98,16 +58,4 @@ public class DoozerTest {
           Arguments.of(testFolder + "googleTest.doozer")
         );
     }
-
-    private static DoozerAction createActionInstance(String actionName, String actionSelector, String actionOptions)
-            throws Exception {
-        String actionClassPrefix = "dev.softtest.doozer.actions.";
-        String actionClassName = actionName.substring(0, 1).toUpperCase()
-                + actionName.substring(1);
-        return (DoozerAction) Class.forName(actionClassPrefix +
-                actionClassName).getConstructor(WebDriver.class, String.class,
-                        String.class, String.class)
-                .newInstance(driver, actionName, actionSelector, actionOptions);
-    }
-
 }
