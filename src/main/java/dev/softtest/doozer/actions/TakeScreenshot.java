d63ed7d4c929a0cd3f34b01c65620d1a475435f3
Mariusz <mkgiepard@gmail.com>
2024-05-03 14:43:54 +0200

    new> wait for a page to be ready before taking screenshot

--- src/main/java/dev/softtest/doozer/actions/TakeScreenshot.java
+++ src/main/java/dev/softtest/doozer/actions/TakeScreenshot.java
@@ -1,10 +1,15 @@
 package dev.softtest.doozer.actions;
 
 import java.io.File;
+import java.time.Duration;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import com.google.common.io.Files;
 import dev.softtest.doozer.DoozerAction;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TakeScreenshot extends DoozerAction {
 
@@ -14,6 +19,9 @@ public class TakeScreenshot extends DoozerAction {
 
     @Override
     public void execute() throws Exception {
+        Wait<WebDriver> wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        wait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+
         byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
         String fileName = getOptions().get("default");
         if (fileName == null) {
