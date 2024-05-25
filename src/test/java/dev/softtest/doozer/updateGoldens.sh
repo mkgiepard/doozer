aeed38494d9d9e088e555d284d631b0236af8dbf
Mariusz <mkgiepard@gmail.com>
2024-05-25 12:02:19 +0200

    infra> script to update goldens in one shot

---
+++ src/test/java/dev/softtest/doozer/updateGoldens.sh
@@ -0,0 +1,7 @@
+export ROOT_DIR="/Users/mario/gitrepos/doozer"
+export RESULTS_DIR="target/doozer-tests"
+
+cd $ROOT_DIR/$RESULTS_DIR
+
+for x in `ls -d *Test`; do cp $x/*.png ../../src/test/java/dev/softtest/doozer/scripts/$x/goldens/;
+rm ../../src/test/java/dev/softtest/doozer/scripts/$x/goldens/*DIFF.png; done
