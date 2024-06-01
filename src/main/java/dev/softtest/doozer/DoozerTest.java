b5a64f5905d4d3d80c8d2aef1870281a5f0dfdf0
Mariusz <mkgiepard@gmail.com>
2024-06-01 15:29:30 +0200

    new> proper logging in parallel execution

--- src/main/java/dev/softtest/doozer/DoozerTest.java
+++ src/main/java/dev/softtest/doozer/DoozerTest.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.time.Duration;
 
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,17 +26,12 @@ import java.util.concurrent.TimeUnit;
 import java.util.Map;
 import java.util.HashMap;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.Layout;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.FileAppender;
-import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.ThreadContext;
 
 @TestInstance(Lifecycle.PER_CLASS)
-// @Execution(ExecutionMode.CONCURRENT)
+@Execution(ExecutionMode.CONCURRENT)
 public abstract class DoozerTest {
     protected static final Logger logger = LogManager.getLogger();
 
@@ -45,14 +41,12 @@ public abstract class DoozerTest {
     @BeforeEach
     public void setup(TestInfo tInfo) throws IOException {
         createResultsDirectories(tInfo.getDisplayName());
-        addTestLogAppender(getResultsDirectory(tInfo.getDisplayName()));
     }
 
     @AfterEach
     public void cleanup(TestInfo tInfo) {
         Context ctx = testCaseRegistry.get(tInfo.getDisplayName()).getContext();
         ctx.getWebDriver().quit();
-        removeTestLogAppender(ctx.getResultsDir());
     }
 
     @AfterAll
@@ -64,6 +58,7 @@ public abstract class DoozerTest {
     @MethodSource("provideDoozerTestFiles")
     public void runner(String testFile, TestInfo tInfo) throws Exception {
         TestCase tc = new TestCase(testFile);
+        ThreadContext.put("ROUTINGKEY", tc.getTestCaseName());
         tc.getContext().setWebDriver(initWebDriver());
         tc.getContext().setResultsDir(getResultsDirectory(tInfo.getDisplayName()));
         testCaseRegistry.put(tInfo.getDisplayName(), tc);
@@ -74,12 +69,17 @@ public abstract class DoozerTest {
         
         long startTime = System.nanoTime();
         tc.readTestScript();
-        tc.run();
-        long duration = System.nanoTime() - startTime;
+        try {
+            tc.run();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            long duration = System.nanoTime() - startTime;
 
-        logger.info("========================== STOP =========================");
-        logger.info("Execution time: " + TimeUnit.NANOSECONDS.toMillis(duration) + "[ms]");
-        logger.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+            logger.info("========================== STOP =========================");
+            logger.info("Execution time: " + TimeUnit.NANOSECONDS.toMillis(duration) + "[ms]");
+            logger.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        }
     }
 
     public abstract Stream<Arguments> provideDoozerTestFiles();
@@ -112,31 +112,6 @@ public abstract class DoozerTest {
                 displayedName.lastIndexOf("/"),
                 displayedName.lastIndexOf(".doozer"))
             + "/";
-    }
-
-    private void addTestLogAppender(String testResultsDir) {
-        final LoggerContext logCtx = (LoggerContext) LogManager.getContext(false);
-        final Configuration config = logCtx.getConfiguration();     
-        
-        @SuppressWarnings("rawtypes")
-        final Layout layout =  config.getRootLogger().getAppenders().get("logFile").getLayout();
-
-        @SuppressWarnings("unchecked")
-        Appender perTestFileAppender = FileAppender.newBuilder()
-            .setName(testResultsDir)
-            .withFileName(testResultsDir + "doozer.log")
-            .setLayout(layout)
-            .withAdvertise(false)
-            .withAppend(false)
-            .build();
-            perTestFileAppender.start();
-        config.getRootLogger().addAppender(perTestFileAppender, Level.INFO, null);
-    }
-
-    private void removeTestLogAppender(String testResultsDir) {
-        final LoggerContext logCtx = (LoggerContext) LogManager.getContext(false);
-        final Configuration config = logCtx.getConfiguration();     
-        config.getRootLogger().removeAppender(testResultsDir);
     }
 
     private void generateReport() {
