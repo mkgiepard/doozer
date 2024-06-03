87324a2abf2f75f28e82327d39851a4b32ed1853
Mariusz <mkgiepard@gmail.com>
2024-06-03 16:30:53 +0200

    infra> remove onFAILURE screenshots after bulk update

--- src/test/java/dev/softtest/doozer/updateGoldens.sh
+++ src/test/java/dev/softtest/doozer/updateGoldens.sh
@@ -3,5 +3,8 @@ export RESULTS_DIR="target/doozer-tests"
 
 cd $ROOT_DIR/$RESULTS_DIR
 
-for x in `ls -d *Test`; do cp $x/*.png ../../src/test/java/dev/softtest/doozer/scripts/$x/goldens/;
-rm ../../src/test/java/dev/softtest/doozer/scripts/$x/goldens/*DIFF.png; done
+for x in `ls -d *Test`;
+do cp $x/*.png ../../src/test/java/dev/softtest/doozer/scripts/$x/goldens/;
+rm ../../src/test/java/dev/softtest/doozer/scripts/$x/goldens/*DIFF.png; 
+rm ../../src/test/java/dev/softtest/doozer/scripts/$x/goldens/*onFAILURE.png;
+done
