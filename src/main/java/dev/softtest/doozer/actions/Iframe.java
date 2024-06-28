78f26b39f89c828666cf622a98fa9359c39641d6
Mariusz <mkgiepard@gmail.com>
2024-06-28 16:54:53 +0200

    javadoc> iframe

--- src/main/java/dev/softtest/doozer/actions/Iframe.java
+++ src/main/java/dev/softtest/doozer/actions/Iframe.java
@@ -4,6 +4,16 @@ import dev.softtest.doozer.Context;
 import dev.softtest.doozer.DoozerAction;
 import dev.softtest.doozer.ElementFinder;
 
+/**
+ * Implements switching between <a href="https://www.selenium.dev/documentation/webdriver/interactions/frames/">
+ * Iframes</a>.
+ * 
+ * <ul>
+ *   <li>By WebElement, if selector is defined</li>
+ *   <li>By Iframe name, name must be given in action args.</li>
+ *   <li>By Iframe index, idex must be given in action args.</li>
+ * </ul>
+ */
 public class Iframe extends DoozerAction {
 
     public Iframe(Context ctx, Integer lineNumber, String actionName, String originalAction) {
@@ -18,8 +28,8 @@ public class Iframe extends DoozerAction {
         if (getOption("name") != "") {
             getDriver().switchTo().frame(getOption("name"));
         }
-        if (getOption("id") != "") {
-            getDriver().switchTo().frame(Integer.parseInt(getOption("id")));
+        if (getOption("index") != "") {
+            getDriver().switchTo().frame(Integer.parseInt(getOption("index")));
         }
         getDriver().switchTo().defaultContent();
     }
