11d6e4c8ac28a102ba9b5b0d69776a753ea98aaa
Mariusz <mkgiepard@gmail.com>
2024-06-29 12:24:21 +0200

    javadoc> import, set

--- src/main/java/dev/softtest/doozer/actions/Import.java
+++ src/main/java/dev/softtest/doozer/actions/Import.java
@@ -3,6 +3,10 @@ package dev.softtest.doozer.actions;
 import dev.softtest.doozer.Context;
 import dev.softtest.doozer.DoozerAction;
 
+
+/**
+ * Dummy class for Import action that is fully served in Parser.
+ */
 public class Import extends DoozerAction {
     
     public Import(Context ctx, Integer lineNumber, String actionName, String originalAction) {
--- src/main/java/dev/softtest/doozer/actions/Set.java
+++ src/main/java/dev/softtest/doozer/actions/Set.java
@@ -3,6 +3,10 @@ package dev.softtest.doozer.actions;
 import dev.softtest.doozer.Context;
 import dev.softtest.doozer.DoozerAction;
 
+
+/**
+ * Set action for adding variables to TestContext.
+ */
 public class Set extends DoozerAction {
 
     public Set(Context ctx, Integer lineNumber, String actionName, String originalAction) {
